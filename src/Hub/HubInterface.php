37796a07caf28bd5328dbdade62042b907ca3b0b
Beno!t POLASZEK <bpolaszek@gmail.com>
2022-05-03 22:44:54 +0200

    refactor: decorrelate HubInterface from TransportInterface

--- src/Hub/HubInterface.php
+++ src/Hub/HubInterface.php
@@ -4,9 +4,26 @@ declare(strict_types=1);
 
 namespace Freddie\Hub;
 
-use Freddie\Hub\Transport\TransportInterface;
+use Freddie\Message\Update;
+use Generator;
+use React\Promise\PromiseInterface;
 
-interface HubInterface extends TransportInterface
+interface HubInterface
 {
     public function getOption(string $name): mixed;
+
+    /**
+     * @return PromiseInterface<Update>
+     */
+    public function publish(Update $update): PromiseInterface;
+
+    public function subscribe(callable $callback): void;
+
+    public function unsubscribe(callable $callback): void;
+
+    /**
+     * @param string $lastEventID
+     * @return Generator<Update>
+     */
+    public function reconciliate(string $lastEventID): Generator;
 }
