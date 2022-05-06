471490a98964f02ffa3bebffcc5b0f296f8e9b19
Beno!t POLASZEK <bpolaszek@gmail.com>
2022-05-06 11:38:27 +0200

    fix: stop loop after each test

--- tests/LoopExtension.php
+++ tests/LoopExtension.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace Freddie\Tests;
 
+use PHPUnit\Runner\AfterTestHook;
 use PHPUnit\Runner\BeforeTestHook;
 use React\EventLoop\Factory;
 use React\EventLoop\Loop;
 
-final class LoopExtension implements BeforeTestHook
+final class LoopExtension implements BeforeTestHook, AfterTestHook
 {
     public function executeBeforeTest(string $test): void
     {
-        Loop::set(Factory::create());
+        //Loop::set(Factory::create());
+    }
+
+    public function executeAfterTest(string $test, float $time): void
+    {
+        Loop::stop();
     }
 }
