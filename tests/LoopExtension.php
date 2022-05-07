e891d2e354c22c6450b5842d12e188564ae9113b
Beno!t POLASZEK <bpolaszek@gmail.com>
2022-05-07 15:54:38 +0200

    fix: create a fresh Loop instance before each test

--- tests/LoopExtension.php
+++ tests/LoopExtension.php
@@ -4,20 +4,14 @@ declare(strict_types=1);
 
 namespace Freddie\Tests;
 
-use PHPUnit\Runner\AfterTestHook;
 use PHPUnit\Runner\BeforeTestHook;
 use React\EventLoop\Factory;
 use React\EventLoop\Loop;
 
-final class LoopExtension implements BeforeTestHook, AfterTestHook
+final class LoopExtension implements BeforeTestHook
 {
     public function executeBeforeTest(string $test): void
     {
-        //Loop::set(Factory::create());
-    }
-
-    public function executeAfterTest(string $test, float $time): void
-    {
-        Loop::stop();
+        Loop::set(Factory::create()); // @see https://github.com/reactphp/event-loop/issues/252
     }
 }
