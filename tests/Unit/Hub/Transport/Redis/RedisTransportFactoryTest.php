f3d2dfde74f003388f335567d8be0e38c086ea48
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-03-15 16:09:19 +0100

    tests(RedisTransportFactoryTest): set pingInterval to 0 so that `RedisTransport` doesn't connect to Redis within the unit test

--- tests/Unit/Hub/Transport/Redis/RedisTransportFactoryTest.php
+++ tests/Unit/Hub/Transport/Redis/RedisTransportFactoryTest.php
@@ -13,7 +13,7 @@ it('supports Redis DSNs', function (string $dsn, bool $expected) {
     $factory = new RedisTransportFactory();
     expect($factory->supports($dsn))->toBe($expected);
 })->with(function () {
-    yield ['redis://localhost', true];
+    yield ['redis://localhost?pingInterval=0.0', true];
     yield ['rediss://some.secure.place.com', true];
     yield ['notredis://shrug', false];
 });
@@ -23,21 +23,22 @@ it('instantiates a Redis transport', function (string $dsn, RedisTransport $expe
     expect($factory->create($dsn))->toEqual($expected);
 })->with(function () {
     $redisFactory = new Factory();
-    yield ['redis://localhost?foo=bar', new RedisTransport(
-        $redisFactory->createLazyClient('redis://localhost?foo=bar'),
-        $redisFactory->createLazyClient('redis://localhost?foo=bar'),
+    yield ['redis://localhost?foo=bar&pingInterval=0.0', new RedisTransport(
+        $redisFactory->createLazyClient('redis://localhost?foo=bar&pingInterval=0.0'),
+        $redisFactory->createLazyClient('redis://localhost?foo=bar&pingInterval=0.0'),
+        options: ['pingInterval' => 0.0],
     )];
-    yield ['redis://localhost?size=1000&trimInterval=2.5', new RedisTransport(
-        $redisFactory->createLazyClient('redis://localhost?size=1000&trimInterval=2.5'),
-        $redisFactory->createLazyClient('redis://localhost?size=1000&trimInterval=2.5'),
-        options: ['size' => 1000, 'trimInterval' => 2.5],
+    yield ['redis://localhost?size=1000&trimInterval=2.5&pingInterval=0.0', new RedisTransport(
+        $redisFactory->createLazyClient('redis://localhost?size=1000&trimInterval=2.5&pingInterval=0.0'),
+        $redisFactory->createLazyClient('redis://localhost?size=1000&trimInterval=2.5&pingInterval=0.0'),
+        options: ['size' => 1000, 'trimInterval' => 2.5, 'pingInterval' => 0.0],
     )];
 });
 
 it('instantiates 2 different clients', function () {
     $factory = new RedisTransportFactory();
     /** @var RedisTransport $transport */
-    $transport = $factory->create('redis://localhost?size=1000');
+    $transport = $factory->create('redis://localhost?size=1000&pingInterval=0.0');
     expect($transport->redis)->toBeInstanceOf(Client::class);
     expect($transport->subscriber)->toBeInstanceOf(Client::class);
     expect($transport->redis)->not()->toBe($transport->subscriber);
