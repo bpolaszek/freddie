6e9a078dd08fcf79661cb9e0cb6cbd8dd0efc622
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-03-19 15:37:16 +0100

    chore: remove unnecessary argument

--- src/Security/JWT/Extractor/QueryTokenExtractor.php
+++ src/Security/JWT/Extractor/QueryTokenExtractor.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace Freddie\Security\JWT\Extractor;
 
-use Freddie\Helper\FlatQueryParser;
 use Psr\Http\Message\ServerRequestInterface;
 
+use function BenTools\QueryString\query_string;
 use function is_string;
 use function strlen;
-use function BenTools\QueryString\query_string;
 
 final class QueryTokenExtractor implements PSR7TokenExtractorInterface
 {
@@ -20,7 +19,7 @@ final class QueryTokenExtractor implements PSR7TokenExtractorInterface
 
     public function extract(ServerRequestInterface $request): ?string
     {
-        $qs = query_string($request->getUri(), new FlatQueryParser());
+        $qs = query_string($request->getUri());
         $authorizationQuery = $qs->getParam($this->name);
         if (!is_string($authorizationQuery) || strlen($authorizationQuery) < 41) {
             return null;
