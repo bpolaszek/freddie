ace8c6e672a53be7404d5579fec8991955027bb1
Dominik Zawadzki <dominik.zawadzki@archdesk.com>
2024-11-01 16:40:55 +0100

    fix code style and integration test

--- tests/Unit/Hub/Middleware/CorsMiddlewareTest.php
+++ tests/Unit/Hub/Middleware/CorsMiddlewareTest.php
@@ -7,8 +7,10 @@ namespace Freddie\Tests\Unit\Hub\Middleware;
 use FrameworkX\App;
 use Freddie\Hub\Middleware\CorsMiddleware;
 use React\Http\Message\Response;
+
 use function expect;
 use function Freddie\Tests\handle;
+use function it;
 
 it('decorate response with Cross-Origin Resource Sharing mechanism', function () {
     // Given
@@ -24,4 +26,22 @@ it('decorate response with Cross-Origin Resource Sharing mechanism', function ()
     expect($response->getHeaderLine('Access-Control-Allow-Origin'))->toBe($corsOrigins);
     expect($response->getHeaderLine('Access-Control-Allow-Methods'))->toBe('*');
     expect($response->getHeaderLine('Access-Control-Allow-Headers'))->toBe('*');
+    expect($response->getHeaderLine('Access-Control-Allow-Credentials'))->toBe('true');
+});
+
+it('decorate response with Cross-Origin Resource Sharing mechanism with default corsOrigin setup', function () {
+    // Given
+    $corsOrigins = null;
+    $expectedResponse = new Response(400);
+    $app = new App(new CorsMiddleware($corsOrigins), fn () => $expectedResponse);
+    $request = new \React\Http\Message\ServerRequest('POST', './well-known/mercure');
+
+    // When
+    $response = handle($app, $request);
+
+    // Then
+    expect($response->getHeaderLine('Access-Control-Allow-Origin'))->toBe('null');
+    expect($response->getHeaderLine('Access-Control-Allow-Methods'))->toBe('*');
+    expect($response->getHeaderLine('Access-Control-Allow-Headers'))->toBe('*');
+    expect($response->getHeaderLine('Access-Control-Allow-Credentials'))->toBe('true');
 });
