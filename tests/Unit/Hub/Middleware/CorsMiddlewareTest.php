9dc978227c2e5f64a2b5f037519991aaa6c8dc0e
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-11-05 15:32:07 +0100

    fix: cosmetics

--- tests/Unit/Hub/Middleware/CorsMiddlewareTest.php
+++ tests/Unit/Hub/Middleware/CorsMiddlewareTest.php
@@ -7,41 +7,42 @@ namespace Freddie\Tests\Unit\Hub\Middleware;
 use FrameworkX\App;
 use Freddie\Hub\Middleware\CorsMiddleware;
 use React\Http\Message\Response;
+use React\Http\Message\ServerRequest;
 
 use function expect;
 use function Freddie\Tests\handle;
 use function it;
 
-it('decorate response with Cross-Origin Resource Sharing mechanism', function () {
+it('decorates response with CORS mechanism', function () {
     // Given
     $corsOrigins = 'http://testdomain.local';
     $expectedResponse = new Response(200);
     $app = new App(new CorsMiddleware($corsOrigins), fn () => $expectedResponse);
-    $request = new \React\Http\Message\ServerRequest('POST', './well-known/mercure');
+    $request = new ServerRequest('POST', './well-known/mercure');
 
     // When
     $response = handle($app, $request);
 
     // Then
-    expect($response->getHeaderLine('Access-Control-Allow-Origin'))->toBe($corsOrigins);
-    expect($response->getHeaderLine('Access-Control-Allow-Methods'))->toBe('*');
-    expect($response->getHeaderLine('Access-Control-Allow-Headers'))->toBe('*');
-    expect($response->getHeaderLine('Access-Control-Allow-Credentials'))->toBe('true');
+    expect($response->getHeaderLine('Access-Control-Allow-Origin'))->toBe($corsOrigins)
+        ->and($response->getHeaderLine('Access-Control-Allow-Methods'))->toBe('*')
+        ->and($response->getHeaderLine('Access-Control-Allow-Headers'))->toBe('*')
+        ->and($response->getHeaderLine('Access-Control-Allow-Credentials'))->toBe('true');
 });
 
-it('decorate response with Cross-Origin Resource Sharing mechanism with default corsOrigin setup', function () {
+it('decorates response with CORS mechanism with default corsOrigin setup', function () {
     // Given
     $corsOrigins = null;
     $expectedResponse = new Response(400);
     $app = new App(new CorsMiddleware($corsOrigins), fn () => $expectedResponse);
-    $request = new \React\Http\Message\ServerRequest('POST', './well-known/mercure');
+    $request = new ServerRequest('POST', './well-known/mercure');
 
     // When
     $response = handle($app, $request);
 
     // Then
-    expect($response->getHeaderLine('Access-Control-Allow-Origin'))->toBe('null');
-    expect($response->getHeaderLine('Access-Control-Allow-Methods'))->toBe('*');
-    expect($response->getHeaderLine('Access-Control-Allow-Headers'))->toBe('*');
-    expect($response->getHeaderLine('Access-Control-Allow-Credentials'))->toBe('true');
+    expect($response->getHeaderLine('Access-Control-Allow-Origin'))->toBe('null')
+        ->and($response->getHeaderLine('Access-Control-Allow-Methods'))->toBe('*')
+        ->and($response->getHeaderLine('Access-Control-Allow-Headers'))->toBe('*')
+        ->and($response->getHeaderLine('Access-Control-Allow-Credentials'))->toBe('true');
 });
